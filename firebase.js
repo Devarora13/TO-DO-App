e5a9ded33ee852a11347ef974ade482ce51755a8
Dev Arora <devarora1309@gmail.com>
2025-07-22 23:03:03 +0530

    Add environment variable configuration and update firebase.js to use dotenv

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage';
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJsghKaFa-qaVOifl-KzU6Bi9fu66b_zs",
-  authDomain: "todo-app-ae281.firebaseapp.com",
-  projectId: "todo-app-ae281",
-  storageBucket: "todo-app-ae281.appspot.com", 
-  messagingSenderId: "805904922637",
-  appId: "1:805904922637:web:c75d8e639e553ec9a4bd44"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
